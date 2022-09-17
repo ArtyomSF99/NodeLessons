245fc76a9fb225fb2cca89102597837ab42ba358
ArtyomSF99 <artyom.gulgulyan.2013@gmail.com>
2022-09-17 12:41:22 +0400

    First commit2

--- lesson5.js
+++ lesson5.js
@@ -1,12 +1,19 @@
-// const http = require("http");
+const http = require("http");
 
-// const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000
 
-// const server = http.createServer((req,res) => {
-//     // res.writeHead(200, {
-//     //     'content-type':'text/html; charset=utf-8; application/json'
-//     // })
-//     // res.end('Сервер работает')                              // можно отправить html разметку
+//statusConde-отправляет статусный код
+//statusMessage-отправляет сообщение вместе с статусным кодом
+//setHeader(name, value)- добавляет в ответ один заголовок
+//write- пишет в поток ответа некоторое содержимое
+//writeHead- добавляет в ответ статусный код и набор заголовков
+//end - сигнализирует серверу, что заголовки и тело ответа установлены, в итоге ответ отсылается клиента
+
+const server = http.createServer((req,res) => {
+    res.writeHead(200, {
+        'content-type':'text/html; charset=utf-8; application/json'
+    })
+    res.end('Сервер работает')                              // можно отправить html разметку
 //     //-----------
 //     if(req.url === '/users'){
 //         return res.end('USERS')
@@ -19,6 +26,6 @@
 //             {id:1, name:'user1'}
 //         ]))
 //     }
-// })
+ })
 
-// server.listen(PORT, () => console.log('Сервер был запущен на порте: ' + PORT ))
+server.listen(PORT, () => console.log('Сервер был запущен на порте: ' + PORT ))
